8fd5ff5decc191cebc689d2fe0cb7111573be8c6
VNetoJunior <volneyjunior000@outlook.com>
2024-10-28 13:00:16 -0300

    Melhorando Projeto

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 
-let nome = "Junior"
-let xp = 100
-let nivel = ""
+let nomeDoHeroi = "Imperador_Junior"
+let xpTotal = 100
+let nivelDoHeroi = " "
+
 
 switch(true) {
     
-    case (xp >= 0 && xp <= 1000) : 
-    nivel = "Ferro"
+    case (xpTotal >= 0 && xpTotal <= 1000) : 
+    nivelDoHeroi = "Ferro"
     break
 
-    case (xp >= 1001 && xp <= 2000) : 
-    nivel = "Bronze"
+    case (xpTotal >= 1001 && xpTotal <= 2000) : 
+    nivelDoHeroi = "Bronze"
     break
 
-    case (xp >= 2001 && xp <= 5000) : 
-    nivel = "Prata"
+    case (xpTotal >= 2001 && xpTotal <= 5000) : 
+    nivelDoHeroi = "Prata"
     break
 
-    case (xp >= 5001 && xp <= 7000) : 
-    nivel = "Ouro"
+    case (xpTotal >= 5001 && xpTotal <= 7000) : 
+    nivelDoHeroi = "Ouro"
     break
 
-    case (xp >= 7001 && xp <= 8000) : 
-    nivel = "Platina"
+    case (xpTotal >= 7001 && xpTotal <= 8000) : 
+    nivelDoHeroi = "Platina"
     break
 
-    case (xp >= 8001 && xp <= 9000) : 
-    nivel = "Ascendente"
+    case (xpTotal >= 8001 && xpTotal <= 9000) : 
+    nivelDoHeroi = "Ascendente"
     break
 
-    case (xp >= 9001 && xp <= 10000) : 
-    nivel = "Imortal"
+    case (xpTotal >= 9001 && xpTotal <= 10000) : 
+    nivelDoHeroi = "Imortal"
     break
 
-    case (xp >= 10001) : 
-    nivel = "Radiante"
+    case (xpTotal >= 10001) : 
+    nivelDoHeroi = "Radiante"
     break
 
     default: (console.log("Erro, esse nível não é válido!"))
 }
 
-console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`)
